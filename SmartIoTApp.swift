2f9cbe5ed1095b044d8983e765824876e1a839ab
Shaundj <38732765+Shaundj@users.noreply.github.com>
2024-06-01 20:59:24 +0800

    Add files via upload

---
+++ SmartIoTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartIoTApp.swift
+//  SmartIoT
+//
+//  Created by Shaun David Jerome on 17/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartIoTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
